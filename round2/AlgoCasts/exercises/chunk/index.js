ddd970aaac2789f9c41e19a44e367f8e4352644a
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2021-05-30 23:08:23 +0900

    done: array chunk

--- round2/AlgoCasts/exercises/chunk/index.js
+++ round2/AlgoCasts/exercises/chunk/index.js
@@ -8,6 +8,43 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const ans = [];
+  let tempArr = [];
+  for (let val of array) {
+    tempArr.push(val);
+    if(tempArr.length === size) {
+      ans.push(tempArr);
+      tempArr = [];
+    }
+  }
+  if(tempArr.length > 0) ans.push(tempArr);
+  return ans;
+}
+
+function chunk2(array, size) {
+    const chunked = [];
+    for (let element of array) {
+        const last = chunked[chunked.length - 1];
+        console.log(last)
+
+        if(!last || last.length === size) {
+            chunked.push([element]);
+        } else {
+            last.push(element);
+        }
+    }
+    return chunked;
+}
+
+function chunk3(array, size) {
+    const chunked = [];
+    let index = 0;
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+    return chunked;
+}
 
 module.exports = chunk;
