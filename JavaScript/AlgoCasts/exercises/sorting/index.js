4cb59792effb84361951285e83f9de8e98c2128c
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2022-03-06 19:22:47 +0900

    done: sorting

--- JavaScript/AlgoCasts/exercises/sorting/index.js
+++ JavaScript/AlgoCasts/exercises/sorting/index.js
@@ -17,15 +17,48 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
 
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const midpoint = Math.floor(arr.length / 2);
+  const left = arr.slice(0, midpoint);
+  const right = arr.slice(midpoint);
+
+  return merge(mergeSort(left), mergeSort(right));
+
 
 }
 
 function merge(left, right) {
+  const reuslts = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      reuslts.push(left.shift());
+    } else {
+      reuslts.push(right.shift())
+    }
+  }
 
+  return [...reuslts, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
